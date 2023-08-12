933e8eaf9bc8c840f137cd2a89e2410e30233064
mnagahama <mnagahama21@gmail.com>
2023-08-12 18:41:35 -0300

    quase no fim

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -22,6 +22,6 @@ public class MainMenu : MonoBehaviour
 
     public void Credits()
     {
-        SceneManager.LoadScene("Creditos");
+        SceneManager.LoadScene("Credits");
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,20 +7,20 @@ public class ScoreManager : MonoBehaviour
     public Text scoreText;
     public Text highScoreText;
     public static int score;
-    public static int highScore;
+    public static int highScore = 0;
     void Start()
-    {
+    {/*
         if (PlayerPrefs.HasKey("HighScore"))
         {
             highScore = PlayerPrefs.GetInt("HighScore");
-        }
+        }*/
     }
     void Update() 
     {
         if (score > highScore)
         {
             highScore = score;
-            PlayerPrefs.SetInt("HighScore", highScore);
+          //PlayerPrefs.SetInt("HighScore", highScore);
         }
         scoreText.text = "Score: " + score;
         highScoreText.text = "High Score: " + highScore;
